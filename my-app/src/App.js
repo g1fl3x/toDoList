f1bcfec19f5b58201ed2f702c6fd8f98b53ddfb8
g1fl3x <antonmalygin22@gmail.com>
2022-01-19 18:49:54 +0300

    add options functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,12 @@ import Sorting from "./components/Sorting";
 import TasksList from "./components/TasksList";
 
 function App() {
+	const [tasks, changeTasks] = useState([
+		{id: "sdfghsdfgfdsg", completed: true, text: "Text example", time: "2022/09/03"},
+		{id: "sdfghsdfgdsfjkg", completed: false, text: "Create reactJs App", time: "2022/01/04"},
+		{id: "isgfgsdfsd", completed: false, text: "Fix code", time: "2021/01/04"}
+	])
+	const [optionsType, setOptionsType] = useState('all')
 
 	function randomString(i) {
 		let rnd = '';
@@ -11,12 +17,6 @@ function App() {
 			rnd += Math.random().toString(36).substring(2);
 		return rnd.substring(0, i);
 	};
-
-	const [tasks, changeTasks] = useState([
-		{id: "sdfghsdfgfdsg", completed: true, text: "Text example", time: "2022/09/03"},
-		{id: "sdfghsdfgdsfjkg", completed: false, text: "Create reactJs App", time: "2022/01/04"},
-		{id: "isgfgsdfsd", completed: false, text: "Fix code", time: "2021/01/04"}
-	])
 
 	function deleteTask(taskId) {
 		changeTasks(tasks.filter(task => task.id !== taskId))
@@ -34,16 +34,47 @@ function App() {
 		changeTasks(tasks.map(task => task.id === taskId ? {...task, completed: complete} : task))
 	}
 
+	function showAllTasks() {
+		setOptionsType('all')
+	}
+
+	function showDoneTasks() {
+		setOptionsType('done')
+	}
+
+	function showUndoneTasks() {
+		setOptionsType('undone')
+	}
+
+	function optionsFilter(item, type) {
+		switch (type) {
+			case 'all':
+				return true
+			case 'done':
+				if (item.completed)
+					return true
+				return false
+			case 'undone':
+				if (item.completed)
+					return false
+				return true	
+		}
+	}
+
   return (
     <div className="centered centered_styles">
 		<header className="header">
 			<h1>ToDo</h1>
 		</header>
 		<AddTask addTaskCallback={addTask} />
-		<Sorting />
+		<Sorting 
+			showAllTasks={showAllTasks}
+			showDoneTasks={showDoneTasks}
+			showUndoneTasks={showUndoneTasks}
+		/>
 		<main className="main">
 			<TasksList 
-				posts={tasks}
+				posts={tasks.filter(item => optionsFilter(item, optionsType))}
 				editTaskCallback={editTask}
 				deleteTaskCallback={deleteTask}
 				completeTaskCallback={completeTask}
--- my-app/src/components/Sorting.jsx
+++ my-app/src/components/Sorting.jsx
@@ -3,13 +3,13 @@ import arrow_down from '../images/icons/arrow_down.svg';
 import OptionsButton from './OptionsButton';
 import SortingButton from './SortingButton';
 
-function Sorting() {
+function Sorting(props) {
     return (
         <aside className="search-form">
             <div className="search-form-buttons">
-                <OptionsButton text="Done" />
-                <OptionsButton text="All" />
-                <OptionsButton text="Undone" />
+                <OptionsButton text="Done" cback={props.showDoneTasks}/>
+                <OptionsButton text="All" cback={props.showAllTasks}/>
+                <OptionsButton text="Undone" cback={props.showUndoneTasks}/>
             </div>
             <div className="search-form-sort">
                 <p className="search-form-sort__text">Sort by Date:</p>
