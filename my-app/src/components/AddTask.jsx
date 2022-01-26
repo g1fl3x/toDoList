525a51b10ea8f2393e3b4d2ea85b4ac4fce472fa
g1fl3x <g1fl3x>
2022-01-26 12:03:11 +0300

    delete input on submit

--- my-app/src/components/AddTask.jsx
+++ my-app/src/components/AddTask.jsx
@@ -1,17 +1,26 @@
 import { Input } from 'antd'
+import { useState } from 'react';
 
 function AddTask({ addTask }) {
 
+    const [inputText, setInputText] = useState("")
+
     function onEnter(text) {
+        setInputText("")
         addTask(text)
+    }
+
+    function handlerOnEdit(e) {
+        setInputText(e.target.value)
     }
 
     return (
         <Input.Search
             placeholder='I want to...'
             onSearch={onEnter}
-            allowClear
+            onChange={handlerOnEdit}
             enterButton='Add'
+            value={inputText}
         />
     );
 }
